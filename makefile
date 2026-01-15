f511f9edad6054bce6a43a2eb0dc2a4ec256a214
Oscar Garnier <ogpourcommander@gmail.com>
2026-01-15 18:28:43 +0100

    first script

--- makefile
+++ makefile
@@ -1,6 +1,10 @@
+
+# Basic training
 train:
-	# Basic training
 	python -m src.train \
 		--output_dir ./my_model \
 		--num_train_epochs 3 \
 		--per_device_train_batch_size 32
+
+remotetrain:
+	sbatch scripts/main_train.sh
---
+++ scripts/main_train.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -p mesonet 
+#SBATCH --account=m25146
+#SBATCH --job-name=train_vgg
+#SBATCH --output=logs/train_vgg%j.out
+#SBATCH --error=logs/train_vgg%j.err
+#SBATCH --time=03:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=marius.henry13@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+# Activate virtual environment
+source .venv/bin/activate
+
+# Run the training script
+python -m src.train \
+    --output_dir ./my_model \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 32
