a858e19ef749d60d65e61e459203b135ebafe8e5
Oscar Garnier <ogpourcommander@gmail.com>
2026-01-18 14:43:48 +0100

    long script for 20

--- training_configs/all_long.py
+++ training_configs/all_long.py
@@ -17,7 +17,7 @@ max_train_samples = None
 val_samples = 5000
 
 output_dir = "./output"
-num_train_epochs = 3
+num_train_epochs = 20
 per_device_train_batch_size = 128
 per_device_eval_batch_size = 128
 learning_rate = 5e-4
